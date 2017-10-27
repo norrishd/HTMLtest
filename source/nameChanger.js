316bf49b7af7f770ef42363bf25cd77ee130431a
David Norrish <u4815128@anu.edu.au>
2017-10-27 12:09:28 +1100

    Add pre-existing files

---
+++ source/nameChanger.js
@@ -0,0 +1,5 @@
+if (document.getElementById("changer").innerHTML == "YOU HAVE CREATED A MONSTER") {
+		document.getElementById("changer").innerHTML = "Just kidding, I'm fine."
+	} else {
+		document.getElementById("changer").innerHTML = "YOU HAVE CREATED A MONSTER";
+	}
